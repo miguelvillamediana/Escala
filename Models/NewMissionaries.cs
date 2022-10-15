14929920660ea5afc35c28c0b0f653c3dd7d0939
famil <famil@MIGUE-LAPTOP>
2022-10-14 22:12:10 -0300

    Add project files.

---
+++ Models/NewMissionaries.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace Escala.Models
+{
+    public class NewMissionaries
+    {
+        public string Idioma { get; set; }
+        public string EmailPessoal { get; set; }
+        public string EmailMissionario { get; set; }
+
+    }
+}
